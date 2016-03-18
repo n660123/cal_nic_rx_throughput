ad2f8cb0562237bd0049cc1a4b698511e0bc4441
Mark Zeng <n660123@gmail.com>
2016-03-18 16:55:27 +0800

    calculation from tx to rx

--- cal_nic.sh
+++ cal_nic.sh
@@ -4,24 +4,20 @@ while [ 1 ]
 do	
 	NIC=wlan0
 
-	eval `cat /proc/net/dev | grep $NIC | cut -d ':' -f 2 | awk '{print "tx_old="$9}'`
-	TXSTARTtime=`date +%s.%N`
-	#echo "tx_old=$tx_old"
-	#echo "TXSTARTtime=$TXSTARTtime"
+	rx_old=$(cat /proc/net/dev | grep $NIC | cut -d ':' -f 2 | awk '{print $1}')
+	RXSTARTtime=`date +%s`
+	echo "rx_old=$rx_old"
+	#echo "RXSTARTtime=$RXSTARTtime"
 	sleep 1
 
-	eval `cat /proc/net/dev | grep $NIC | cut -d ':' -f 2 | awk '{print "tx="$9}'`
-	TXENDtime=`date +%s.%N`
-	if [ $tx_old -gt $tx ]; then 
-		tx=$((4294967296+$rx))
-		#echo "Count NIC stop byte:  " $tx
-	#else
-		#echo "Count NIC stop byte:  " $tx
-	fi
-	#echo "tx=$tx"
-	#echo "TXENDtime=$TXENDtime"
-	TX_diff=`echo "scale=9; ((((($tx-$tx_old)/1024)/1024)*8)/($TXENDtime-$TXSTARTtime))" | bc -l`
-	echo "$TX_diff"
+	rx=$(cat /proc/net/dev | grep $NIC | cut -d ':' -f 2 | awk '{print $1}')
+	RXENDtime=`date +%s`
+	echo "rx=$rx"
+	#echo "RXENDtime=$RXENDtime"
+
+	rx_diff=$(($rx-$rx_old))
+	#X_diff=`echo "scale=9; ((((($tx-$tx_old)/1024)/1024)*8)/($TXENDtime-$TXSTARTtime))" | bc -l`
+	echo "$rx_diff"
 
 	#sleep 1
 done
